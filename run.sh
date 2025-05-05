e188731516284c318419fd4078ecbd165da883eb
kroll1349 <kroll1349@gmail.com>
2025-05-05 22:35:17 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,3 +3,15 @@
 echo "Add-on pornit!"
 # Poți adăuga orice aplicație sau script care trebuie să ruleze
 tail -f /dev/null
+import requests
+
+url = "https://api.opencloud.com/data"
+headers = {"Authorization": "Bearer YOUR_API_KEY"}
+
+response = requests.get(url, headers=headers)
+
+if response.status_code == 200:
+    data = response.json()
+    print(data)
+else:
+    print("Eroare la cererea API:", response.status_code)
